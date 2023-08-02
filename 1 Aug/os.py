9141f6ddd483e640e10680c1f09bb7de1bd7fe86
Sakshi Sharma <sakshiq18@gmail.com>
2023-08-02 10:01:09 +0530

    Assignment completed

--- 1 Aug/os.py
+++ 1 Aug/os.py
@@ -7,7 +7,7 @@ inp=int(input("Enter a choice (1-4) : "))
 if(inp==1):
     print(datetime.datetime.now())
 elif(inp==2):
-    path = "Desktop"
+    path = "C:\\Users\\user\\Desktop"
     dir_list = os.listdir(path) 
     print("Files and directories in '", path, "' :")  
     # print the list 
@@ -15,13 +15,13 @@ elif(inp==2):
 elif(inp==3):
     directory = "PythonLinux"
     # Parent Directory path 
-    parent_dir = "Desktop"
+    parent_dir = "C:\\Users\\user\\Desktop"
     # Path 
     path = os.path.join(parent_dir, directory)
     os.mkdir(path) 
     print("Directory '% s' created" % directory) 
 elif(inp==4):
-    #pywhatkit.sendwhatmsg("+918005524478","Ji!",20, 30)
+    pywhatkit.sendwhatmsg("+918005524478","Ji!",20, 30)
     print("pywhatkit sent message succesfully!")
 else:
     print("Enter valid input!")
